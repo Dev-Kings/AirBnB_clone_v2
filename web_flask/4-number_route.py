b3d52889b62d56290d887142c00b928b9d8ad746
Dev-Kings <kingasiadavid41@gmail.com>
2024-07-27 21:08:30 +0300

    added route number/<int:n>

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,58 +1,59 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /: Displays 'Hello HBNB!'.
-    /hbnb: Displays 'HBNB'.
-    /c/<text>: Displays 'C' followed by the value of <text>.
-    /python/(<text>): Displays 'Python' followed by the value of <text>.
-    /number/<n>: Displays 'n is a number' only if <n> is an integer.
-"""
+""" Module with script to start a Flask Web application """
 from flask import Flask
-from flask import abort
 
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """Displays 'Hello HBNB!'."""
-    return "Hello HBNB!"
+    """Returns:
+            String Hello HBNB!
+    """
+    return ("Hello HBNB!")
 
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """Displays 'HBNB'."""
-    return "HBNB"
-
-
-@app.route("/c/<text>", strict_slashes=False)
-def c(text):
-    """Displays 'C' followed by the value of <text>.
-
-    Replaces any underscores in <text> with slashes.
+    """Returns
+            HBNB
     """
-    text = text.replace("_", " ")
-    return "C {}".format(text)
+    return ("HBNB")
 
 
-@app.route("/python", strict_slashes=False)
-@app.route("/python/<text>", strict_slashes=False)
-def python(text="is cool"):
-    """Displays 'Python' followed by the value of <text>.
-
-    Replaces any underscores in <text> with slashes.
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """ Replaces _ with space if text argument has it.
+        Args:
+            text (str) : Text to print
+        Returns:
+            C <text>
     """
-    text = text.replace("_", " ")
-    return "Python {}".format(text)
+    return (f"C {text.replace('_', ' ')}")
 
 
-@app.route("/number/<int:n>", strict_slashes=False)
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_is_fun(text):
+    """ Replaces _ with space if text argument has it.
+        Args:
+            text (str) : Text to print
+        Returns:
+            Python <text>
+    """
+    return (f"Python {text.replace('_', ' ')}")
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
-    """Displays 'n is a number' only if n is an integer."""
-    return "{} is a number".format(n)
+    """Displays text if n is an integer
+        Args:
+            n (int) : Number to display
+        Returns:
+            <n> is a number
+    """
+    return(f"{n} is a number")
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)
