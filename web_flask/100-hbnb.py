5815c990d676be2580559acefb46cf8792c867c2
Dev-Kings <kingasiadavid41@gmail.com>
2024-08-01 15:02:33 +0300

    HBNB fetches States, Cities, Amenities, Places, User from db

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -8,8 +8,12 @@ Routes:
 from models import storage
 from flask import Flask
 from flask import render_template
+from os import getenv
 
 app = Flask(__name__)
+
+# Determine the type of storage being used
+storage_type = getenv('HBNB_TYPE_STORAGE')
 
 
 @app.route("/hbnb", strict_slashes=False)
@@ -18,8 +22,9 @@ def hbnb():
     states = storage.all("State")
     amenities = storage.all("Amenity")
     places = storage.all("Place")
-    return render_template("100-hbnb.html",
-                           states=states, amenities=amenities, places=places)
+    return render_template("100-hbnb.html", states=states,
+                           amenities=amenities, places=places,
+                           storage_type=storage_type)
 
 
 @app.teardown_appcontext
